087c02ce3687cc057428404dd171c7f778fc89bb
danamoreno05 <159939303+danamoreno05@users.noreply.github.com>
2026-03-12 11:58:48 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Sistema LendWise
+# Proyecto Final Algoritmia y Programación
+
+def menu():
+    print("=== SISTEMA LENDWISE ===")
+    print("1. Registrar usuario")
+    print("2. Registrar ítem")
+    print("3. Registrar préstamo")
+    print("4. Registrar devolución")
+    print("5. Consultar artículos prestados")
+    print("6. Administrador")
+    print("7. Salir")
+
+menu()
